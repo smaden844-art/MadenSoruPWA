e2b44a937d0cfde428e94b6bee117ba17284ed06
smaden844-art <smaden844@gmail.com>
2026-08-04 19:46:13 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,7 @@
-const CACHE_NAME = "maden-soru-arsivi-v1";
-const FILES = [
-  "./",
-  "./index.html",
-  "./manifest.json"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-  );
-  self.skipWaiting();
+const CACHE="maden-pwa-v1";
+self.addEventListener("install",e=>{
+e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./manifest.json"])));
 });
-
-self.addEventListener("activate", event => {
-  event.waitUntil(self.clients.claim());
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener("fetch",e=>{
+e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));
 });
